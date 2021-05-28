e4893fd812450c1e2bdf4c2b1c36359c4a948cca
ivgng <mohamadirvanfajarali@gmail.com>
2021-05-28 13:40:17 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        return User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+    }
+
+    public function login(Request $request)
+    {
+        if(!Auth::attempt($request->only('email','password'))){
+            return response([
+                'message' => 'akun tidak ada!'
+            ], Response::HTTP_UNAUTHORIZED);
+        }    
+
+        $user = Auth::user();
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        $cookie = cookie('jwt', $token, 60 * 24); //1 hari
+        
+        return response([
+            'message' => $token
+        ])->withCookie($cookie);
+    }
+
+    public function user()
+    {
+        return Auth::user();
+    }
+
+    public function logout()
+    {
+        $cookie = Cookie::forget('jwt');
+
+        return response([
+            'message' => 'Berhasil Logout'
+        ])->withCookie($cookie);
+    }
+}
